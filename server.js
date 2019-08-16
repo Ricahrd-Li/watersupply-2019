5208bde9037afe852365cb7641974f343c1523e8
Zhehao Li <1003449832@qq.com>
2019-08-16 14:40:57 -0500

    solve the problem of css

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+var path = require('path');
 
 //For Heroku 
 let port = process.env.PORT;
@@ -10,24 +11,25 @@ if(port == null || port == ""){
 //A simple server
     http.createServer(function(req, res){
         console.log(req.url);
-        if(req.url === "/"){
-            fs.readFile("./public/index.html", "UTF-8", function(err, html){
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(html);
-            });
-        }else if(req.url.match("\.css$")){
-            var cssPath = path.join(__dirname, 'public', req.url);
-            var fileStream = fs.createReadStream(cssPath, "UTF-8");
-            res.writeHead(200, {"Content-Type": "text/css"});
-            fileStream.pipe(res);
-    
-        }else if(req.url.match("\.png$")){
-            var imagePath = path.join(__dirname, 'public', req.url);
-            var fileStream = fs.createReadStream(imagePath);
-            res.writeHead(200, {"Content-Type": "image/png"});
-            fileStream.pipe(res);
-        }else{
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.end("No Page Found");
+        switch (req.url){
+            case "/style.css" :
+                fs.readFile("./public/style.css", "UTF-8", function(err, css){
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    res.end(css);
+                });
+                break;
+
+            case "/USTC.png":
+                fs.readFile("./public/USTC.png", function(err,img){
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    res.end(img);
+                });
+                break;
+
+            default: 
+                fs.readFile("./public/index.html", "UTF-8", function(err, html){
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.end(html);
+                });
         }
 }).listen(port);
