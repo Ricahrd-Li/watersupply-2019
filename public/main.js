3c574bd0ef6d5543dea487b770b93744aee35cf5
Zhehao Li <1003449832@qq.com>
2019-08-17 00:00:52 -0500

    adjust css: 1. must use frame 2. communication between front and end

--- public/main.js
+++ public/main.js
@@ -1,16 +1,19 @@
 // import { isNumber } from "util"; --> not work
+var currentNumEast = 10;
+var currentNumWest = 15;
+var currentNumMid = 0;
 
 function summitNumEast(){
     // document.getElementById("numEast").innerHTML = "hi";
     var numEast = document.getElementById("numEast").value;
     // if (typeof(numEast)!="number") alert("错误！缺水数应当是数字！");
-    alert(numEast +" 提交成功");
+    alert(numEast +"(东区) 提交成功");
 }
 function summitNumWast(){
     var numWest = document.getElementById("numWest").value;
-    alert("提交成功");
+    alert(numWest +"(西区) 提交成功");
 }
 function summitNumMiddle(){
-    var numMiddle = document.getElementById("numMiddle").value;
-    alert("提交成功");
+    var numMid = document.getElementById("numMid").value;
+    alert(numMid +"(中区) 提交成功");
 }
